6d7149d78289335742e928856c759b6e7481db01
NiniDev <linantfin@gmail.com>
2023-05-31 16:06:33 +0200

    Added Pagination functionality

--- app/views/clients.py
+++ app/views/clients.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, redirect, request, url_for
 from flask import render_template
 from app import db
 from app.models.models import *
@@ -8,5 +8,9 @@ clients = Blueprint("clients", __name__)
 
 @clients.route("/clients")
 def clients_page():
-    clients = db.session.query(Person).filter(Person.our_client == True).limit(16)
+    page = request.args.get("page", 1, type=int)
+    select = db.session.query(Person).filter(Person.our_client == True)
+    clients = db.paginate(select, page=page, per_page=16, error_out=False)
+    if page > clients.pages:
+        return redirect(url_for("clients.clients_page", page=clients.pages))
     return render_template("clients.html", clients=clients)
--- app/views/lawyers.py
+++ app/views/lawyers.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, redirect, request, url_for
 from flask import render_template
 from app import db
 from app.models.models import *
@@ -8,5 +8,9 @@ lawyers = Blueprint("lawyers", __name__)
 
 @lawyers.route("/lawyers")
 def lawyers_page():
-    lawyers = db.session.query(Person).filter(Person.our_lawyer == True).limit(16)
+    page = request.args.get("page", 1, type=int)
+    select = db.session.query(Person).filter(Person.our_lawyer == True)
+    lawyers = db.paginate(select, page=page, per_page=16, error_out=False)
+    if page > lawyers.pages:
+        return redirect(url_for("lawyers.lawyers_page", page=lawyers.pages))
     return render_template("lawyers.html", lawyers=lawyers)
