02c9b8ed76da79a76c733dc2535dd356c398ca28
bigomato <buchnertim512@gmail.com>
2023-06-03 16:02:57 +0200

    finished create_client

--- app/views/clients.py
+++ app/views/clients.py
@@ -2,6 +2,7 @@ from flask import Blueprint, redirect, request, url_for
 from flask import render_template
 from app import db
 from app.models.models import *
+from datetime import datetime
 
 clients = Blueprint("clients", __name__)
 
@@ -16,6 +17,30 @@ def clients_page():
     return render_template("clients.html", clients=clients)
 
 
-@clients.route("/clients/create")
+@clients.route("/clients/create", methods=["GET", "POST"])
 def create_client():
+    if request.method == "POST":
+        a = Address(
+            city=request.form.get("city"),
+            street=request.form.get("street"),
+            house_number=request.form.get("house_number"),
+            zip_code=request.form.get("zip_code"),
+            country=request.form.get("country"),
+        )
+
+        p = Person(
+            name=request.form.get("name"),
+            surname=request.form.get("surname"),
+            birthdate=datetime.strptime(request.form.get("birthdate"), "%Y-%m-%d"),
+            our_client=True,
+            address=a,
+        )
+
+        ci = ContactInfo(
+            phone_number=request.form.get("phone_number"),
+            email=request.form.get("email"),
+            person=p,
+        )
+        db.session.add_all([a, p, ci])
+        db.session.commit()
     return render_template("create_client.html")
