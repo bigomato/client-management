f25e288a687c33a0c6fc99578b4115e787db8344
NiniDev <linantfin@gmail.com>
2023-06-10 14:00:35 +0200

    Add create person for directly adding to the case

--- app/views/persons.py
+++ app/views/persons.py
@@ -14,8 +14,29 @@ persons = Blueprint("persons", __name__)
 def create_person():
     type = request.args.get("type")
     form = CreatePersonForm()
+    case_id = request.args.get("case_id")
+    case = Case.query.filter_by(id=case_id).first()
+    role = request.args.get("role")
+    if not case and case_id not in ["", None]:
+        flash(
+            "Du hast versucht eine Person zu einem Fall hinzuzufügen, der nicht existiert.",
+            "danger",
+        )
+        return redirect(url_for("cases.cases_page"))
+    if case and not role:
+        flash(
+            "Du hast versucht eine Person zu einem Fall hinzuzufügen, ohne eine Rolle anzugeben.",
+            "danger",
+        )
+        return redirect(url_for("cases.edit_case_involved", case_id=case_id))
+    # check if the role is valid using the Enum
+    if case and role not in [role.name for role in InvolvementRole]:
+        flash(
+            "Du hast versucht eine Person zu einem Fall hinzuzufügen, mit einer ungültigen Rolle.",
+            "danger",
+        )
+        return redirect(url_for("cases.edit_case_involved", case_id=case_id))
     if form.validate_on_submit():
-        print("Hi")
         a = Address(
             city=form.city.data,
             street=form.street.data,
@@ -28,8 +49,12 @@ def create_person():
             name=form.name.data,
             surname=form.surname.data,
             birthdate=form.birthdate.data,
-            our_client=True if request.form.get("lawyer_or_client")=="client" else False,
-            our_lawyer=True if request.form.get("lawyer_or_client")=="lawyer" else False,
+            our_client=True
+            if request.form.get("lawyer_or_client") == "client"
+            else False,
+            our_lawyer=True
+            if request.form.get("lawyer_or_client") == "lawyer"
+            else False,
             address=a,
         )
 
@@ -39,9 +64,20 @@ def create_person():
             person=p,
         )
 
-        print("a, p, ci", a, p, ci)
+        # check if they wanted to add a person to a case
+        if case_id != "":
+            inv = Involved(
+                case_id=case_id,
+                person=p,
+                role=role,
+            )
+            db.session.add(inv)
+
         db.session.add_all([a, p, ci])
         db.session.commit()
-        print("a, p, ci", a, p, ci)
+
+        if case_id != "":
+            flash("Die Person wurde erfolgreich hinzugefügt.", "success")
+            return redirect(url_for("cases.edit_case_involved", case_id=case_id))
 
     return render_template("create_person.html", form=form, type=type)
