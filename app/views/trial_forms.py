eb2d7f47d7378ddb35cfc7d2093eb0b31e202632
NiniDev <linantfin@gmail.com>
2023-06-14 19:22:31 +0200

    Now can add judgement to trial

--- app/views/trial_forms.py
+++ app/views/trial_forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, DateField
+from wtforms import StringField, BooleanField, DateField, SelectField
 from wtforms.validators import DataRequired, Length
+
+from app.models.models import JudgemenType
 
 
 class EditTrialForm(FlaskForm):
@@ -29,3 +31,18 @@ class EditTrialForm(FlaskForm):
     zip_code = StringField("Postleitzahl", description="Postleitzahl")
     street = StringField("Straße", description="Straße")
     house_number = StringField("Hausnummer", description="Hasunummer")
+
+    judgement_description = StringField(
+        "Beschreibung",
+        description="Beschreibung",
+    )
+    judgement = SelectField(
+        "Urteil",
+        choices=[(status.name, status.display()) for status in JudgemenType],
+        description="Urteil",
+    )
+    document = SelectField(
+        "Dokument",
+        choices=[],
+        description="Dokument",
+    )
--- app/views/trials.py
+++ app/views/trials.py
@@ -1,3 +1,4 @@
+import datetime
 from flask import Blueprint, flash, redirect, request, url_for, send_file
 from flask import render_template, current_app
 from werkzeug.utils import secure_filename
@@ -31,6 +32,10 @@ def delete_trial(trial_id):
 def edit_trial(trial_id):
     form = EditTrialForm()
     trial = db.session.query(Trial).get_or_404(trial_id)
+    docs = db.session.query(Document).filter(Document.case_id == trial.case_id).all()
+    form.document.choices = [("none", "Kein Dokument")] + [
+        (doc.id, str(doc)) for doc in docs
+    ]
     if form.validate_on_submit():
         trial.name = form.name.data
         trial.date = form.date.data
@@ -41,6 +46,22 @@ def edit_trial(trial_id):
             trial.address.zip_code = form.zip_code.data
             trial.address.street = form.street.data
             trial.address.house_number = form.house_number.data
+        if trial.judgement is None:
+            judgement = Judgement(
+                description=form.judgement_description.data,
+                judgement=form.judgement.data,
+                document_id=form.document.data
+                if form.document.data != "none"
+                else None,
+                date=datetime.datetime.now(),
+            )
+            trial.judgement = judgement
+        else:
+            trial.judgement.description = form.judgement_description.data
+            trial.judgement.judgement = form.judgement.data
+            trial.judgement.document_id = (
+                form.document.data if form.document.data != "none" else None
+            )
         db.session.commit()
         flash("Die Verhandlung wurde erfolgreich bearbeitet.", "success")
         return redirect(url_for("cases.edit_case_trials", case_id=trial.case_id))
@@ -55,6 +76,10 @@ def edit_trial(trial_id):
             form.zip_code.data = trial.address.zip_code
             form.street.data = trial.address.street
             form.house_number.data = trial.address.house_number
+        if trial.judgement is not None:
+            form.judgement_description.data = trial.judgement.description
+            form.judgement.data = trial.judgement.judgement.name
+            form.document.data = trial.judgement.document_id
 
     return render_template(
         "edit_trial.html",
